f1c7ef0ca2e2e896e6a770bb3a30cbec0dab8660
Johan Abildskov <randomsort@gmail.com>
2020-05-03 13:54:31 +0200

    Add fast-forward exercise script

---
+++ chapter4/fast-forward.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf fast-forward/
+git init fast-forward
+cd fast-forward
+
+echo "first file" > 0.txt
+git add 0.txt
+git commit -m "Initial Commit"
+
+echo "\n additional content" >> 0.txt
+git add .
+git commit -m "second commit"
+
