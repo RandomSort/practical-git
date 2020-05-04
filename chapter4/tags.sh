496b0cb9e2f1d07f02bd1f9569dfd3faa0015dd4
Johan Abildskov <randomsort@gmail.com>
2020-05-04 10:40:56 +0200

    [Ch4] Add tag exercise

---
+++ chapter4/tags.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rf tags/
+git init tags
+cd tags
+
+echo "first file" > 0.txt
+git add 0.txt
+git commit -m "Initial Commit"
+
+echo "\n additional content" >> 0.txt
+git add .
+git commit -m "second commit"
+
+git branch feature
+
+touch master.txt
+git add master.txt
+git commit -m "Add master.txt"
+
+git checkout feature
+touch feature.txt
+git add feature.txt
+git commit -m "Add feature.txt"
+
